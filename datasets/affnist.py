d2b71cc76d88ec9a7d5be2e20548493c37a3e292
matthiasware <matthias.mitterreiter@uni-jena.de>
2021-12-02 11:40:52 +0100

    added AUTO download for AffNIST

--- datasets/affnist.py
+++ datasets/affnist.py
@@ -1,18 +1,18 @@
 from pathlib import Path
+import hashlib
 
+# external libraries
 import torch
 from torch.utils.data import Dataset
+
+# local
+from .download_utils import download_file_from_google_drive
 
 
 class AffNIST(Dataset):
     """
-        Requires files in p_root:
-        - affnist_train.pt
-        - affnist_valid.pt
-        - mnist_train.pt
-        - mnist_valid.pt
-
-        CHECK NOTEBOOK TO CREATE THE DATASET
+        CHECK NOTEBOOK TO CREATE THE DATASET FROM SCRATCH
+        IF DOWNLOAD DOES NOT WORK
     """
 
     DATA_DIR = "AFFNIST"
@@ -22,8 +22,26 @@ class AffNIST(Dataset):
         'affnist_train': 'affnist_train.pt',
         'affnist_valid': 'affnist_valid.pt'
     }
+    FILE_LINKS = {
+        "mnist_train": 'https://drive.google.com/file/d/1ugK4SBt-GNjET3fTrozV3UEuZ_tC_yjJ/view?usp=sharing',
+        "mnist_valid": 'https://drive.google.com/file/d/1Puc7tQgBJ9Vl_2Zjbdp6EUXs6T6Jz_7I/view?usp=sharing',
+        "affnist_train": 'https://drive.google.com/file/d/1EilN9NiDh_rcL-SjDYbM4oEVxIXBCc6t/view?usp=sharing',
+        "affnist_valid": 'https://drive.google.com/file/d/1BNIvJBRdwDQdzhznv9A8P8hL0iIKDNUF/view?usp=sharing'
+    }
+    FILE_LINK_ID = {
+        "mnist_train": '1ugK4SBt-GNjET3fTrozV3UEuZ_tC_yjJ',
+        "mnist_valid": '1Puc7tQgBJ9Vl_2Zjbdp6EUXs6T6Jz_7I',
+        "affnist_train": '1EilN9NiDh_rcL-SjDYbM4oEVxIXBCc6t',
+        "affnist_valid": '1BNIvJBRdwDQdzhznv9A8P8hL0iIKDNUF'
+    }
+    FILE_CHECKSUMS = {
+        "mnist_train": '5646df9ccec5ec450919a2e2db453c96',
+        "mnist_valid": '05fb76ad01cc2c1fd5cc22926b95c901',
+        "affnist_train": '2849b72ca42795ef2ee99094b9255b9a',
+        "affnist_valid": '297649ec1df45bea6920ff24818ef607'
+    }
 
-    def __init__(self, p_root: str, split: str = 'mnist_train', transform=None, target_transform=None):
+    def __init__(self, p_root: str, split: str = 'mnist_train', transform=None, target_transform=None, download=True):
         """
             p_root:
                 path to directory where directory AFFNIST resides
@@ -45,6 +63,9 @@ class AffNIST(Dataset):
         self.split = split
         self.transform = transform
         self.target_transform = target_transform
+
+        if download is True:
+            self.download_files()
 
         if self.split not in self.SPLIT_FILES:
             raise Exception("split: '{}' does not exist!".format(self.split))
@@ -77,3 +98,29 @@ class AffNIST(Dataset):
             target = self.target_transform(target)
 
         return img, target
+
+    def download_files(self):
+        for split in self.SPLIT_FILES.keys():
+            # check if alread exists on drive
+            p_file = Path(self.p_root) / self.DATA_DIR / \
+                self.SPLIT_FILES[split]
+            if p_file.exists():
+                continue
+
+            print("Downloading {} from file {}".format(
+                split, self.FILE_LINKS[split]))
+            # create data directory
+            p_file.parents[0].mkdir(exist_ok=True, parents=True)
+
+            # download file
+            file_id = self.FILE_LINK_ID[split]
+            download_file_from_google_drive(file_id, p_file)
+
+            # checksum
+            checksum_exp = self.FILE_CHECKSUMS[split]
+
+            checksum_act = hashlib.md5(open(p_file, 'rb').read()).hexdigest()
+
+            if not checksum_act == checksum_exp:
+                raise Exception(
+                    "Checksums do not match for split {}!".format(split))
