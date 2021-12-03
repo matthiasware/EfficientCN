15744b32c2ff1439c436358f2b94aaaf33ba3093
Sören Laue <soeren.laue@uni-jena.de>
2021-12-03 09:30:05 +0100

    added basline - no hyperparams

---
+++ mnist_baseline_cnn_fast.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms as T
+import torch.nn as nn
+from torch import optim
+
+import numpy as np
+from effcn.models import MnistBaselineCNN
+from effcn.utils import count_parameters
+
+if torch.cuda.is_available():  
+    dev = "cuda:0" 
+else:  
+    dev = "cpu"  
+# dev = 'cpu'
+device = torch.device(dev)
+
+print("Using device: {}".format(device))
+
+
+if __name__ == '__main__':
+    ds_train = datasets.MNIST(root='./data', train=True, download=True, transform=T.ToTensor())
+    ds_valid = datasets.MNIST(root="./data", train=False, download=True, transform=T.ToTensor())
+
+    batch_size_train = ds_train.__len__() // 1
+    batch_size_valid = ds_valid.__len__() // 1
+    dl_train = torch.utils.data.DataLoader(ds_train, 
+                                           batch_size=batch_size_train, 
+                                           shuffle=False, 
+                                           num_workers=1)
+    dl_valid = torch.utils.data.DataLoader(ds_valid, 
+                                           batch_size=batch_size_valid, 
+                                           shuffle=False, 
+                                           num_workers=1)
+
+    x_train, y_train = next(iter(dl_train))
+    x_valid, y_valid = next(iter(dl_valid))
+    
+    x_train = x_train.to(device)
+    y_train = y_train.to(device)
+    
+    x_valid = x_valid.to(device)
+    y_valid = y_valid.to(device)
+
+    model = MnistBaselineCNN()
+    model = model.to(device)
+
+    print("#params: {}".format(count_parameters(model)))
+
+    loss_func = nn.CrossEntropyLoss()
+#    optimizer = optim.Adam(model.parameters(), lr = 0.01)
+#    lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.96)
+    optimizer = optim.LBFGS(model.parameters(), history_size=10, max_iter=1, 
+                            line_search_fn="strong_wolfe")
+
+    print("Train ...", flush="True")
+    num_epochs = 150
+    model.train()
+    for epoch in range(num_epochs):
+        def closure():
+            optimizer.zero_grad()
+            y_pred = model(x_train)
+            loss = loss_func(y_pred, y_train)         
+            loss.backward()
+            return loss
+
+        optimizer.step(closure)
+
+        y_pred_train = model(x_train)
+        loss = loss_func(y_pred_train, y_train)         
+        y_pred_train = torch.max(y_pred_train, 1)[1]
+        correct_train = (y_pred_train == y_train).sum().item()
+        acc_train = correct_train / y_train.shape[0]
+        
+        y_pred_valid = model(x_valid)
+        y_pred_valid = torch.max(y_pred_valid, 1)[1]
+        correct_valid = (y_pred_valid == y_valid).sum().item()
+        acc_valid = correct_valid / y_valid.shape[0]
+            
+        print("Epoch[{}/{}]  loss: {:.6f}  train_acc: {:.4f}  test_acc: {:.4f}".format(epoch, num_epochs, loss.item(), acc_train, acc_valid))
+
+    """
+    print("Eval ...", flush="True")
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for x, y_true in dl_valid:
+            x = x.to(device)
+            y_true = y_true.to(device)
+            y_pred = model(x)
+            y_pred = torch.max(y_pred, 1)[1]
+            correct += (y_pred == y_true).sum().item()
+            total += y_true.shape[0]
+        acc = correct / total
+
+    print("Accuracy @Test:      {:.3f}".format(acc))
+    print("num total err @Test: {}".format(total - correct))
+    """
