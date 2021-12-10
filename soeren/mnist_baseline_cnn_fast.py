e2ae83aedfedd4cc763e031f6446a56000bbd66e
Sören Laue <soeren.laue@uni-jena.de>
2021-12-10 12:31:46 +0100

    moved full dataset to gpu for speed

--- soeren/mnist_baseline_cnn_fast.py
+++ soeren/mnist_baseline_cnn_fast.py
@@ -25,6 +25,9 @@ print("Using device: {}".format(device))
 
 
 if __name__ == '__main__':
+    torch.manual_seed(0)
+    torch.backends.cudnn.benchmark = True
+
     ds_train = datasets.MNIST(root='./../data', train=True, download=True, transform=T.ToTensor())
     ds_valid = datasets.MNIST(root="./../data", train=False, download=True, transform=T.ToTensor())
 
@@ -33,19 +36,27 @@ if __name__ == '__main__':
     dl_train = torch.utils.data.DataLoader(ds_train, 
                                            batch_size=batch_size_train, 
                                            shuffle=False, 
-                                           num_workers=8)
+                                           num_workers=2)
     dl_valid = torch.utils.data.DataLoader(ds_valid, 
                                            batch_size=batch_size_valid, 
                                            shuffle=False, 
-                                           num_workers=8)
+                                           num_workers=2)
     y_total_train = []
-    for _, y_train in dl_train:
+    dl_train_2 = []
+    for x_train, y_train in dl_train:
         y_total_train += y_train
+        x_train = x_train.to(device)
+        y_train = y_train.to(device)
+        dl_train_2.append((x_train, y_train))
     y_total_train = torch.stack(y_total_train)
-    y_total_train = y_total_train.to(device)
+#    y_total_train = y_total_train.to(device)
+
+    dl_valid_2 = []
+    for x_valid, y_valid in dl_valid:
+        x_valid = x_valid.to(device)
+        y_valid = y_valid.to(device)
+        dl_valid_2.append((x_valid, y_valid))
     
-    torch.manual_seed(0)
-    torch.backends.cudnn.benchmark = True
     model = MnistBaselineCNN()
     model = model.to(device)
 
@@ -64,9 +75,9 @@ if __name__ == '__main__':
             optimizer.zero_grad()
             y_pred_list = []
             total_loss = torch.tensor(0., requires_grad=False, device=device)
-            for idx, (x_train, y_train) in enumerate(dl_train):
-                x_train = x_train.to(device)
-                y_train = y_train.to(device)
+            for idx, (x_train, y_train) in enumerate(dl_train_2): # train):
+#                x_train = x_train.to(device)
+#                y_train = y_train.to(device)
                 y_pred = model(x_train)
                 loss = loss_func(y_pred, y_train)         
                 loss.backward()
@@ -79,6 +90,7 @@ if __name__ == '__main__':
             return total_loss, y_pred, idx + 1
 
         loss, y_predict = optimizer.step(closure)
+        print('#')
         
         with torch.no_grad():
 #            y_pred_train = model(x_train)
@@ -90,9 +102,9 @@ if __name__ == '__main__':
             
             correct_valid = 0
             total_valid = 0
-            for x_valid, y_valid in dl_valid:
-                x_valid = x_valid.to(device)
-                y_valid = y_valid.to(device)
+            for x_valid, y_valid in dl_valid_2:
+#                x_valid = x_valid.to(device)
+#                y_valid = y_valid.to(device)
                 y_pred_valid = model(x_valid)
                 y_pred_valid = torch.max(y_pred_valid, 1)[1]
                 correct_valid += (y_pred_valid == y_valid).sum().item()
