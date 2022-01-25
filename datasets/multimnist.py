b54f629e02daa1af1147c6c874515e9bb0595bc2
Marcel Koch <marcelkoch91@t-online.de>
2022-01-25 12:33:28 +0100

    mmnist train test

--- datasets/multimnist.py
+++ datasets/multimnist.py
@@ -1,5 +1,7 @@
 import sys
 sys.path.append("./..")
+sys.path.append(".")
+
 import os
 
 from pathlib import Path
@@ -21,12 +23,12 @@ import numpy as np
 from dotted_dict import DottedDict
 
 #local imports
-#from misc.utils import mkdir_directories
+from misc.utils import mkdir_directories
 
 
-def mkdir_directories(dirs, parents, exist_ok):
-    for director in dirs:
-        Path(director).mkdir(parents=parents, exist_ok=exist_ok)
+#def mkdir_directories(dirs, parents, exist_ok):
+#    for director in dirs:
+#        Path(director).mkdir(parents=parents, exist_ok=exist_ok)
 
 
 class MultiMNist(Dataset):
@@ -38,7 +40,6 @@ class MultiMNist(Dataset):
             self.p_data  = self.p_root / "Train"
         else:
             self.p_data  = self.p_root / "Valid"
-        self.p_img = self.p_data / "Img"
 
         #transforms
         self.transform = transform
@@ -46,30 +47,27 @@ class MultiMNist(Dataset):
         
         #generation
         if generate == True:
-            self.generator(n_train=g_samples[0], n_valid=g_samples[1])
+            self.__generator__(g_samples=g_samples)
         
         #path exist check
         if not self.__check_exists__():
             raise RuntimeError('Dataset on choosen file not found or corrupted.' +
                                ' You can use generate=True to generate from MNist to choosen file')
 
+
+        #load mnist dataset
+        if train==True:
+            self.mnist_ds = datasets.MNIST(root=self.p_root, train=True, download=False, transform=T.ToTensor())
+        else:
+            self.mnist_ds = self.mnist_valid = datasets.MNIST(root=self.p_root, train=False, download=False, transform=T.ToTensor())
+        self.labels = self.mnist_ds.targets
         #load target files
-        file = open(self.p_data / 'targets_1.plk', 'rb')
-        self.targets_1 = pickle.load(file)
-        file.close()
+        self.mnist_idx_1, self.mnist_idx_2, self.padding_1, self.padding_2 = self.__load_meta__()
 
-        file = open(self.p_data / 'targets_2.plk', 'rb')
-        self.targets_2 = pickle.load(file)
-        file.close()
-        
-        #load list of imgs
-        self.data = os.listdir(self.p_img)
-
-    def __getitem__(self, idx):
-        data_loc = os.path.join(self.p_img, self.data[idx])
-        x = Image.open(data_loc).convert("L")
-        y = self.targets_1[idx]
-        z = self.targets_2[idx]
+    def __getitem__(self, mmn_idx):
+        x = self.__mm_clamp__(mmn_idx)
+        y = self.labels[self.mnist_idx_1[mmn_idx]]
+        z = self.labels[self.mnist_idx_2[mmn_idx]]
         
         if self.transform is not None:
             x = self.transform(x)
@@ -81,128 +79,125 @@ class MultiMNist(Dataset):
         return x, y, z
     
     def __len__(self):
-        return len(self.data)
+        return len(self.mnist_idx_1)
 
     def __check_exists__(self):
         """ Check if processed files exists."""
         files = (
             self.p_root,
-            self.p_data,
-            self.p_img
+            self.p_data
         )
         fpaths = [os.path.exists(f) for f in files]
         return False not in fpaths
 
-    def generator(self,n_train=1000, n_valid=1000):
+    def __load_meta__(self):
+        #load meta profil for mmnist
+        file1 = open(self.p_data / 'idx_1.pkl', 'rb')
+        id_1 = pickle.load(file1)
+        file1.close()
+
+        file2 = open(self.p_data / 'idx_2.pkl', 'rb')
+        id_2 = pickle.load(file2)
+        file2.close()  
+
+        file3 = open(self.p_data / 'padding_1.pkl', 'rb')
+        padding_1 = pickle.load(file3)
+        file3.close()
+
+        file4 = open(self.p_data / 'padding_2.pkl', 'rb')
+        padding_2 = pickle.load(file4)
+        file4.close()
+
+        return id_1, id_2, padding_1, padding_2 
+
+    def __mm_clamp__(self, mmn_idx):
+        base  = T.Pad(padding=self.padding_1[mmn_idx])(self.mnist_ds[self.mnist_idx_1[mmn_idx]][0])
+        top   = T.Pad(padding=self.padding_2[mmn_idx])(self.mnist_ds[self.mnist_idx_2[mmn_idx]][0])
+        merge = torch.clamp(base + top, min=0, max=1)
+        return merge
+
+    def __generator__(self,g_samples=[1000,1000]):
         #mnist
         ds_train = datasets.MNIST(root=self.p_root, train=True, download=True, transform=T.ToTensor())
         ds_valid = datasets.MNIST(root=self.p_root, train=False, download=True, transform=T.ToTensor())
 
-        print('generate multimnist...')
-        
-        self. __multimatch__(p_data=self.p_root / "Train", images=ds_train.data, labels=ds_train.targets,n=n_train)
-        self. __multimatch__(p_data=self.p_root / "Valid", images=ds_valid.data, labels=ds_valid.targets,n=n_valid)
-
+        print('generate multimnist training data...')
+        self. __multimatch__(p_data=self.p_root / "Train", data = ds_train,n=g_samples[0])
+        print('generate multimnist valid data...')
+        self. __multimatch__(p_data=self.p_root / "Valid", data = ds_valid,n=g_samples[1])
         print('generation done')
 
-    def __pad_rand__(self):
+    def __pad_rand_l__(self, m,n):
+        #generate random pos for padding
         ref = 8
-        left = np.random.randint(1, high=9, size=None, dtype=int)
+        left = np.random.randint(1, high=9, size=(m*n), dtype=int)
         rigth = ref - left
-        up = np.random.randint(1, high=9, size=None, dtype=int)
+        up = np.random.randint(1, high=9, size=(m*n), dtype=int)
         down = ref - up
-        
-        return [left, up, rigth, down]
-    
-    def __multimatch__(self, p_data, images, labels, n=1000):
-        #paths
-        p_imgs = p_data / 'Img'
-        
-        mkdir_directories([p_data, p_imgs], parents=True, exist_ok=True)
 
-        #lists
-        all_targets1 = []
-        all_targets2 = []
+        l_stack = np.stack((left,up, rigth, down), axis=1).tolist()
         
-        #generator index
-        index = 1
-        
-        #test dataset
-        #test = images[0:600]
-        #print(test.size())
-        #for j, image in enumerate(test):
-        
-        #generate for whole dataset
-        for j, image in enumerate(images):
-        
-            #reference img
-            img_ref = images[j]
-            lab_ref = labels[j]
+        return l_stack
 
-            #choose random top images from different classes
-            top_idx = np.where(labels != lab_ref)[0]
-            top_idx = np.random.choice(top_idx,n,replace=False)
-
-            #generate images
-            for i, idx in enumerate(top_idx):
-                
-                #randomize position
-                base  = T.Pad(padding=self.__pad_rand__())(images[j])
-                top   = T.Pad(padding=self.__pad_rand__())(images[top_idx[i]])
-                
-                #merge images
-                merge = torch.clamp(base + top,min=0, max=1)
-                merge = merge.unsqueeze(0)
-                
-                #add labels to list
-                label1 = labels[j]
-                label2 = labels[top_idx[i]]
-                all_targets1.append(label1)
-                all_targets2.append(label2)
-
-                #Save Img as png
-                torchvision.utils.save_image(merge.float(), p_imgs / "{:08d}.png".format(index))
-                index += 1 
+    def __multimatch__(self, p_data, data, n=1000):
         
-        #create target 1
-        file_targets1 = open(p_data /'targets_1.plk', 'wb')
-        pickle.dump(all_targets1, file_targets1)
-        file_targets1.close()
-        
+        #labels
+        labels=data.targets
+
+        # proof path
+        mkdir_directories([self.p_root, p_data], parents=True, exist_ok=True)
+
+        #generate index list for mmnist
+        all_ref = []
+        all_top = []
+
+        #generate top label idx for each class
+        all_label = np.unique(labels)
+        top_idx = []
+        for k in all_label:
+            top_idx.append(np.where(labels != k)[0])
+
+        #generate merging labels
+        for j, label in enumerate(labels):
+            list_top = np.random.choice(top_idx[label.item()],n,replace=False).tolist()
+            list_ref = np.full(n,j).tolist()
+            all_ref.extend(list_ref)
+            all_top.extend(list_top)
+
+        #create target 1 id's
+        file1 = open(p_data /'idx_1.pkl', 'wb')
+        pickle.dump(all_ref, file1)
+        file1.close()
+
         #create target 2
-        file_targets2 = open(p_data /'targets_2.plk', 'wb')
-        pickle.dump(all_targets2, file_targets2)
-        file_targets2.close()
+        file2 = open(p_data /'idx_2.pkl', 'wb')
+        pickle.dump(all_top, file2)
+        file2.close()
+
+        #create rand padding pos
+        padding1 = self.__pad_rand_l__(len(labels),n)
+        padding2 = self.__pad_rand_l__(len(labels),n)
+
+        #create padding 1 
+        file3 = open(p_data /'padding_1.pkl', 'wb')
+        pickle.dump(padding1, file3)
+        file3.close()
+
+        #create padding 2 
+        file4 = open(p_data /'padding_2.pkl', 'wb')
+        pickle.dump(padding2, file4)
+        file4.close()
 
 
-
-def loadtrageds():
-    #load target files
-    file = open('/mnt/data/datasets/test/Valid/targets_1.plk', 'rb')
-    targets_1 = pickle.load(file)
-    file.close()
-
-    file = open('/mnt/data/datasets/test/Valid/targets_2.plk', 'rb')
-    targets_2 = pickle.load(file)
-    file.close()  
-
-    for i, target in enumerate(targets_1):
-    	print(i+1, targets_1[i], targets_2[i])
 
 if __name__ == '__main__':
     print('example')
 
-    #A = MultiMNist(root='/mnt/data/datasets/multimnist_test',train=True)#, generate=True, g_samples=[20,10])
-    #print(A[1])
-    #B = MultiMNist(root='/mnt/data/datasets/multimnist_test',train=False)#, generate=True, g_samples=[20,10])
-    #print(B[1])
-    MultiMNist(root='/mnt/data/datasets/multimnist_test',train=True, generate=True, g_samples=[10,10])
-    #loadtrageds()
+    A = MultiMNist(root='/mnt/data/datasets/multimnist_test',train=True, generate=True, g_samples=[10,10])
 
-    #create target 1
-    file_targets1 = open('/mnt/data/datasets/multimnist11/done.plk', 'wb')
-    pickle.dump('Done', file_targets1)
-    file_targets1.close()
+    print(A)
+    print(A[0]) 
+
 
 
 
